60b657369043b0684edbe1d05edf3ce41972b663
jose junior <jniostrc@gmail.com>
2020-11-17 11:51:18 -0300

    Implementado os metados de editar o campo departamento

--- src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/mballem/curso/boot/web/controller/DepartamentoController.java
@@ -2,8 +2,9 @@ package com.mballem.curso.boot.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -24,8 +25,9 @@ public class DepartamentoController {
 	 
 	 
 	 @GetMapping("/listar")
-	 public String listar() {
+	 public String listar(ModelMap model) {
 		 
+		 model.addAttribute("departamentos", service.buscarTodos());
 		 return "/departamento/lista";
 	 }
 	 
@@ -36,5 +38,22 @@ public class DepartamentoController {
 		 
 		 return "redirect:/departamentos/cadastrar";
 		 
+	 }
+	 
+	 @GetMapping("/editar/{id}")
+	 public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		 
+		 model.addAttribute("departamento", service.buscarPorId(id));
+		 
+		 return "/departamento/cadastro";
+		 
+	 }
+	 
+	 @PostMapping("/editar")
+	 public String editar(Departamento departamento) {
+		 
+		 service.editar(departamento);
+		 
+		 return "redirect:/departamentos/cadastrar";
 	 }
 }
